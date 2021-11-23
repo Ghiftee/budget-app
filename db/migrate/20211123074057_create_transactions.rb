7df4f085573de2838b96a7686dd5c57843d31a6b
Ghiftee <gigibetine@gmail.com>
2021-11-23 08:46:37 +0100

    Create schema

--- db/migrate/20211123074057_create_transactions.rb
+++ db/migrate/20211123074057_create_transactions.rb
@@ -1,7 +1,9 @@
 class CreateTransactions < ActiveRecord::Migration[6.1]
   def change
     create_table :transactions do |t|
-
+      t.string :name
+      t.decimal :amount
+      t.references :author, references: :users, null: false, foreign_key: { to_table: :users }
       t.timestamps
     end
   end
