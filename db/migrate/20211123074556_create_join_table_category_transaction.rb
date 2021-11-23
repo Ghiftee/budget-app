879feb35d823c43dd79c9fb81442b672adb335ae
Ghiftee <gigibetine@gmail.com>
2021-11-23 09:17:54 +0100

    Install devise

--- db/migrate/20211123074556_create_join_table_category_transaction.rb
+++ db/migrate/20211123074556_create_join_table_category_transaction.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableCategoryTransaction < ActiveRecord::Migration[6.1]
   def change
-    create_join_table :categories, :deals do |t|
-      t.index [:category_id, :deal_id]
+    create_join_table :categories, :transactions do |t|
+      t.index [:category_id, :transaction_id]
     end
   end
 end
