8711ad53166bc6647f05b7687243afa400d357a6
Ghiftee <gigibetine@gmail.com>
2021-11-24 09:55:52 +0100

    Modify categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,15 +8,14 @@ class CategoriesController < ApplicationController
   end
 
   # GET /categories/1 or /categories/1.json
-  def show; end
+  def show
+    @activities = @category.activities.order(created_at: 'desc')
+  end
 
   # GET /categories/new
   def new
     @category = Category.new
   end
-
-  # GET /categories/1/edit
-  def edit; end
 
   # POST /categories or /categories.json
   def create
@@ -43,7 +42,7 @@ class CategoriesController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_category
-    @category = Category.find(params[:id])
+    @category = Category.includes(:activities).find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
