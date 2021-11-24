583b783ef4f20a69c0345eba77dd8fce4d56f345
Ghiftee <gigibetine@gmail.com>
2021-11-24 16:19:35 +0100

    Test splash request

--- spec/features/splash_spec.rb
+++ spec/features/splash_spec.rb
@@ -7,12 +7,12 @@ RSpec.feature 'Splash page', type: :feature do
     end
 
     scenario 'I can see the login button' do
-      click_link 'Login'
+      click_link 'LOGIN'
       expect(current_path).to eq(new_user_session_path)
     end
 
     scenario 'I can see the sign up button' do
-      click_link 'Sign up'
+      click_link 'SIGN UP'
       expect(current_path).to eq(new_user_registration_path)
     end
   end
