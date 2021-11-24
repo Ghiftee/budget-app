5f2b866de455215d3d2759a449f6f5ac523d8b74
Ghiftee <gigibetine@gmail.com>
2021-11-24 16:54:39 +0100

    Test categories request

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   load_and_authorize_resource
-  before_action :set_category, only: %i[show destroy]
+  before_action :set_category, only: %i[show]
 
   # GET /categories or /categories.json
   def index
@@ -27,14 +27,6 @@ class CategoriesController < ApplicationController
       else
         format.html { render :new, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /categories/1 or /categories/1.json
-  def destroy
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
     end
   end
 
