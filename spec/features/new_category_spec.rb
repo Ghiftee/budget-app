f39098febb7d3e8c59316e978a83faf830c0dff1
Ghiftee <gigibetine@gmail.com>
2021-11-24 14:14:39 +0100

    Create test structure

---
+++ spec/features/new_category_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.feature 'New Category', type: :feature do
+  scenario 'I can see the username and password inputs and the Submit button' do
+    visit new_user_session_path
+    expect(page.has_field?('user_email')).to be true
+    expect(page.has_field?('user_password')).to be true
+    expect(page.has_button?('Log in')).to be true
+  end
+end
---
+++ spec/requests/categories_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe '/categories', type: :request do
+  
+end
