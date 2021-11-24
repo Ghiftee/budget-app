3d413944a22396d8f2fd87f64a1122c4279212f7
Ghiftee <gigibetine@gmail.com>
2021-11-24 15:59:13 +0100

    Test add transaction

--- spec/features/new_transaction_spec.rb
+++ spec/features/new_transaction_spec.rb
@@ -1,32 +1,32 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature 'Add Transaction', type: :feature do
-#   login_user
+RSpec.feature 'Add Transaction', type: :feature do
+  login_user
 
-#   background do
-#     @category = FactoryBot.create(:category, user: @user)
-#   end
+  background do
+    @category = FactoryBot.create(:category, user: @user)
+  end
 
-#   given(:activity) { FactoryBot.build(:activity) }
+  given(:activity) { FactoryBot.build(:activity) }
 
-#   scenario 'Transaction with valid inputs' do
-#     visit new_activity_path
-#     within 'form' do
-#       fill_in 'Name', with: activity.name
-#       fill_in 'Amount', with: activity.amount
-#       select @category.name
-#     end
-#     click_button 'Save'
-#     expect(page).to have_current_path category_path(@category)
-#   end
+  scenario 'Transaction with valid inputs' do
+    visit new_activity_path
+    within 'form' do
+      fill_in 'Name', with: activity.name
+      fill_in 'Amount', with: activity.amount
+      select @category.name
+    end
+    click_button 'Save'
+    expect(page).to have_current_path category_path(@category)
+  end
 
-#   scenario 'Category with invalid inputs' do
-#     visit new_activity_path
-#     within 'form' do
-#       fill_in 'Name', with: activity.name
-#       fill_in 'Amount', with: activity.amount
-#     end
-#     click_button 'Save'
-#     expect(page).to have_content "Categories can't be blank"
-#   end
-# end
+  scenario 'Category with invalid inputs' do
+    visit new_activity_path
+    within 'form' do
+      fill_in 'Name', with: activity.name
+      fill_in 'Amount', with: activity.amount
+    end
+    click_button 'Save'
+    expect(page).to have_content "Categories can't be blank"
+  end
+end
