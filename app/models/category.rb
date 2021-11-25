2a0f89754e70a8fbe19106805f359603eaf29291
Ghiftee <gigibetine@gmail.com>
2021-11-25 12:29:21 +0100

    Rollback changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true
-  validates :icon, presence: true
+  validates :icon, presence: true, length: { maximum: 2 }
 
   belongs_to :user
   has_and_belongs_to_many :activities
