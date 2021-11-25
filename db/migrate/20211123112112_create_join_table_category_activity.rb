fb5bcb323f80506b0751b7e40a0b1438d8fcb397
Gift Uwhubetine <gigibetine@gmail.com>
2021-11-25 12:36:07 +0100

    Revert "Ruby on Rails capstone project - Budget app"

--- db/migrate/20211123112112_create_join_table_category_activity.rb
+++
@@ -1,7 +0,0 @@
-class CreateJoinTableCategoryActivity < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :categories, :activities do |t|
-      t.index [:category_id, :activity_id]
-    end
-  end
-end
